e514101a57c6f9089bb1a971dcb05f18f2ea998c
osss2046 <oscarboris@outlook.cl>
2023-05-24 16:47:08 -0400

    Initial commit

---
+++ src/Arbol.java
@@ -0,0 +1,12 @@
+public class Three {
+
+    double alturaFt;
+    double diametroTroncoInches;
+    TipoArbol tipoArbol;
+
+    th
+    void crecer(){
+        this.alturaFt = this.alturaFt+10;
+        this.diametroTroncoInches= this.diametroTroncoInches+1;
+    }
+}
